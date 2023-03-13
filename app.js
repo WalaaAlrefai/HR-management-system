dde0c8ae5f769d5d8b3f6d4a645e0787db568aab
WalaaAlrefai <lulusedqi@gmail.com>
2023-03-13 07:18:59 +0000

    fifished

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 `use strict`
 const allEmployee=[]
 let secEl=document.getElementById("sec1");
-let secEl2=document.getElementById("sec2");
-let secEl3=document.getElementById("sec3");
-let secEl4=document.getElementById("sec4");
 let form=document.getElementById("form");
 
-function Employee (fullName,department,level,imageUrl,salary) {
+function Employee (employeeId,fullName,department,level,imageUrl,salary) {
+    this.employeeId=employeeId;
     this.fullName=fullName;
     this.department=department;
     this.level=level;
     this.imageUrl=imageUrl;
-    this.salary=salary;
+    this.salary=0;
     allEmployee.push(this)
 }
 
-// let firstEmployee=new Employee("1000","Ghazi Samer","Administration","Senior","assets/Ghazi.jpg");
-// let secondEmployee=new Employee("1001","Lana Ali","Finance","Senior","assets/Lana.jpg");
-// let thirdEmployee=new Employee("1002","Tamara Ayoub","Marketing","Senior","assets/Tamara.jpg");
-// let forthEmployee=new Employee("1003","Safi Walid","Administration","Mid-Senior","assets/Safi.jpg");
-// let fifthEmployee=new Employee("1004","Omar Zaid","Development","Senior","assets/Omar.jpg");
-// let sixthEmployee=new Employee("1005","Rana Saleh","Development","Junior","assets/Rana.jpg");
-// let seventhEmployee=new Employee("1006","Hadi Ahmad","Finance","Mid-Senior","assets/Hadi.jpg");
+let firstEmployee=new Employee("1000","Ghazi Samer","Administration","Senior","assets/Ghazi.jpg");
+let secondEmployee=new Employee("1001","Lana Ali","Finance","Senior","assets/Lana.jpg");
+let thirdEmployee=new Employee("1002","Tamara Ayoub","Marketing","Senior","assets/Tamara.jpg");
+let forthEmployee=new Employee("1003","Safi Walid","Administration","Mid-Senior","assets/Safi.jpg");
+let fifthEmployee=new Employee("1004","Omar Zaid","Development","Senior","assets/Omar.jpg");
+let sixthEmployee=new Employee("1005","Rana Saleh","Development","Junior","assets/Rana.jpg");
+let seventhEmployee=new Employee("1006","Hadi Ahmad","Finance","Mid-Senior","assets/Hadi.jpg");
 
 Employee.prototype.calculateSalary= function (){
     if(this.level=="Senior"){
@@ -32,60 +30,47 @@ Employee.prototype.calculateSalary= function (){
         return Math.floor(Math.random() * (1000 - 500) ) + 500;   
     }
 }
+
 Employee.prototype.netSalary= function (){
-  
+  console.log(this.calculateSalary());
    let tax =this.calculateSalary()*0.075;
    let netSal=this.calculateSalary()-tax;
+   console.log(netSal);
    this.salary=netSal;
-   return netSal;
 }
 
 Employee.prototype.render=function(){
+    this.netSalary();
+    console.log('hi');
+    let divEl=document.createElement("div");
+    secEl.appendChild(divEl);
     let imgEmp=document.createElement("img");
     imgEmp.src=this.imageUrl;
-    secEl.appendChild(imgEmp);
+    divEl.appendChild(imgEmp);
     let h4El=document.createElement("h4");
     h4El.textContent=`Name: ${this.fullName} - ID: ${this.getEmpId()}`;
-    secEl.appendChild(h4El);
+    divEl.appendChild(h4El);
     let h4El2=document.createElement("h4");
     h4El2.textContent=`Department: ${this.department} - Level: ${this.level}.`;
-    secEl.appendChild(h4El2);
+    divEl.appendChild(h4El2);
     let h4El3=document.createElement("h4");
-    h4El3.textContent=this.netSalary()
-    secEl.appendChild(h4El3);
-      
+    h4El3.textContent=this.salary;
+    divEl.appendChild(h4El3);
+    
 }
 
-for (let i = 0; i < allEmployee.length; i++) {
-    allEmployee[i].render();
+
+
+function uniqueId(){
+
+    return (Math.floor(Math.random() * 10000));
 }
+
 Employee.prototype.getEmpId=function(){
-       let empId;
-    switch(this.fullName){
-        case "Ghazi Samer":
-            empId=1000;
-            break;
-        case "Lana Ali":
-            empId=1001;
-            break;
-        case "Tamara Ayoub":
-            empId=1002;
-            break;
-        case "Safi Walid":
-            empId=1003;
-            break;
-        case "Omar Zaid":
-            empId=1004;
-            break;
-        case "Rana Saleh":
-            empId=1005;
-            break;
-        case "Hadi Ahmad":
-            empId=1006;
-            break;
-    }
-    this.employeeId=empId;
-    return empId ;
+       
+    this.employeeId=uniqueId();
+    console.log(uniqueId());
+   
 }
 
 
@@ -95,7 +80,24 @@ function submitHandler(event) {
     let flName=(event.target.fname.value);
     let dept=(event.target.department.value);
     let levelVar=(event.target.level.value);
-    let imgVar=(event.target.img.value);
-    let showNewEmployee=new Employee(flName,dept,levelVar,imgVar);
+    let imgVar=(event.target.image.value);
+    let showNewEmployee=new Employee(0,flName,dept,levelVar,imgVar);
     showNewEmployee.render();
+    console.log(showNewEmployee);
+    
+    
 }
+function allEmployeeCaller(){
+
+
+    
+    for(let i=0 ;i<allEmployee.length;i++){
+        allEmployee[i].getEmpId();
+        console.log(allEmployee[i]);
+        allEmployee[i].render();
+       
+    }
+    
+}
+allEmployeeCaller(allEmployee);
+
